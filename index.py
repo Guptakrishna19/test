ad4b024472cf0dca728531010547b6207113d22a
Krishna <krishnag20001@gmail.com>
2025-02-17 16:03:00 +0530

    changes in index page

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print('hello')
+def add_numbers(a, b):
+    return a + b
+
+def concat_strings(str1, str2):
+    return str1 + str2
+
+print('hello world')
+print(add_numbers(3, 5))
+print(concat_strings('hello', ' world'))

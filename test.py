539dac6c3513a8e16d847ca4c31dde98c959609e
Krishna <krishnag20001@gmail.com>
2025-02-17 15:57:25 +0530

    added some code

---
+++ test.py
@@ -0,0 +1,16 @@
+from index import add_numbers, concat_strings
+
+def test_add_numbers():
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
+
+def test_concat_strings():
+    assert concat_strings('foo', 'bar') == 'foobar'
+    assert concat_strings('hello', ' world') == 'hello world'
+    assert concat_strings('', '') == ''
+
+if __name__ == "__main__":
+    test_add_numbers()
+    test_concat_strings()
+    print("All tests passed.")
